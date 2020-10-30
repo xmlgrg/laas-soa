b049ff1f35676f95effcb3943a8c418b63a8b4b6
tristan <tanshilinmail@gmail.com>
2020-10-30 23:37:57 +0800

    temp

--- component/mysql_.go
+++ component/mysql_.go
@@ -8,33 +8,34 @@ import (
 	"strconv"
 )
 
-var db sql.DB
-
 // 初始化mysql
-func Init(ip string, port int, username string, password string, dbName string) {
+func InitMysql(host string, port int, username string, password string, dbName string) *sql.DB {
 	//"用户名:密码@[连接方式](主机名:端口号)/数据库名"
-	connInfo := username + ":" + password + "@(" + ip + ":" + strconv.Itoa(port) + ")/" + dbName
+	connInfo := username + ":" + password + "@(" + host + ":" + strconv.Itoa(port) + ")/" + dbName
 	db, _ := sql.Open("mysql", connInfo) // 设置连接数据库的参数
 	defer db.Close()                     //关闭数据库
 	err := db.Ping()                     //连接数据库
 	if err != nil {
 		panic(err)
 	}
+	return db
 }
 
 // 查询
-func Query(sql string, args ...interface{}) {
-	rows, _ := db.Query(sql, args) //获取所有数据
-	list.New()
+func QueryMysql(db *sql.DB, sql string, args ...interface{}) *list.List {
+	rows, _ := db.Query(sql, args)
+	result := list.New()
 	var id, name string
 	for rows.Next() { //循环显示所有的数据
 		rows.Scan(&id, &name)
 		fmt.Println(id, "--", name)
 	}
+	return result
 }
 
 // 变更
-func Change() {
+func ChangeMysql() bool {
+	var result = true
 	/*
 	   stu:=[2][2] string{{"3","ketty"},{"4","rose"}}
 	   stmt,_:=db.Prepare("insert into stu values (?,?)")      //获取预处理语句对象
@@ -42,4 +43,5 @@ func Change() {
 	       stmt.Exec(s[0],s[1])            //调用预处理语句
 	   }
 	*/
+	return result
 }
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"github.com/laashub-soa/laas-soa/component"
 	"log"
@@ -8,12 +9,15 @@ import (
 )
 
 var config = component.LoadConfig()
+var db *sql.DB
 var globalActionChanMap = map[string]chan string{} // 全局消息数据
 
 func main() {
+	db = component.InitMysql(config.Get("mysql.host").(string), config.Get("mysql.port").(int), config.Get("mysql.username").(string), config.Get("mysql.password").(string), config.Get("mysql.db_name").(string))
+	log.Println(db)
 	flag.Parse()
 	log.SetFlags(0)
-	log.Println("server started")
+	log.Println("server started at: http://localhost:8080")
 	InitRoute()
 	var addr = flag.String("addr", "localhost:8080", "http service address")
 	log.Fatal(http.ListenAndServe(*addr, nil))
