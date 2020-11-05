7aa717ed4d1eacde1ae5780aaa6faf0c78d4478a
tristan <tanshilinmail@gmail.com>
2020-11-05 21:21:58 +0800

    优化\数据预热.md 应用场景\数据库热升级\数据库热升级.md 初始化工作流基础业务

---
+++ rest/workflow/data.py
@@ -0,0 +1,64 @@
+"""
+流程业务执行(动态)
+
+"""
+
+
+def compose():
+    """
+    流程编排
+    镜像 > 容器
+    编排
+
+    存在介于 流程模板 和 流程实例 之间, 是创建流程的第一步, 为用户提供可用流程人员编排
+    :return:
+    """
+    pass
+
+
+def select():
+    """
+    查询流程
+    :return:
+    """
+    pass
+
+
+def create():
+    """
+    创建流程
+    :return:
+    """
+    pass
+
+
+def flow():
+    """
+    流动流程
+    :return:
+    """
+    pass
+
+
+def rollback():
+    """
+    回滚流程
+    :return:
+    """
+    pass
+
+
+def reject():
+    """
+    驳回流程
+    :return:
+    """
+    pass
+
+
+def destroy():
+    """
+    销毁流程
+    :return:
+    """
+    pass
