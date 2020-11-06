c3c3ec9fdc6412e0425472b80f59a2af95c95bbd
tristan <tanshilinmail@gmail.com>
2020-11-06 14:31:30 +0800

    完善 流程审核.md 添加数据库表结构 table_struct.sql

--- rest/workflow/table_struct.sql
+++ rest/workflow/table_struct.sql
@@ -1,39 +1,68 @@
-CREATE TABLE `workflow_struct`
+create table workflow_struct
 (
-    `id`          int(11)   NOT NULL AUTO_INCREMENT,
-    `title`         int(11)   NOT NULL,
-    `did`         int(11)   NOT NULL,
-    `file`        mediumtext COLLATE utf8mb4_unicode_ci,
-    `update_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    PRIMARY KEY (`id`)
-) ENGINE = InnoDB
-  AUTO_INCREMENT = 1
-  DEFAULT CHARSET = utf8mb4
-  COLLATE = utf8mb4_unicode_ci;
+    id              int auto_increment comment '自增主键, 同时作为版本号'
+        primary key,
+    service_type    varchar(100)                        not null comment '业务类型',
+    workflow        varchar(255)                        not null comment '流程中的角色的审核顺序',
+    create_by       varchar(100)                        not null comment '创建人',
+    create_datetime timestamp default CURRENT_TIMESTAMP not null comment '创建时间'
+)
+    comment '流程模板';
 
-CREATE TABLE `workflow_data`
+create table workflow_data
 (
-    `id`          int(11)   NOT NULL AUTO_INCREMENT,
-    `lid`         int(11)   NOT NULL,
-    `did`         int(11)   NOT NULL,
-    `file`        mediumtext COLLATE utf8mb4_unicode_ci,
-    `update_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    PRIMARY KEY (`id`)
-) ENGINE = InnoDB
-  AUTO_INCREMENT = 1
-  DEFAULT CHARSET = utf8mb4
-  COLLATE = utf8mb4_unicode_ci;
+    id              int auto_increment comment '自增主键'
+        primary key,
+    title           varchar(255)                        not null comment '标题',
+    service_type    varchar(255)                        not null comment '业务类型',
+    form_data_id    int                                 not null comment '表单数据id',
+    form_data       mediumtext                          not null comment '表单数据',
+    steps           varchar(255)                        not null comment '整体审核人流程',
+    next_steps      varchar(255)                        not null comment '剩余审核人流程',
+    next_step       varchar(100)                        not null comment '下一个审核人',
+    group_id        int                                 null comment '流程组的id',
+    execute_order   int                                 null comment '执行顺序',
+    status          varchar(100)                        not null comment '流程状态',
+    service_status  varchar(100)                        not null comment '业务执行状态',
+    create_by       varchar(100)                        not null comment '创建人',
+    create_datetime timestamp default CURRENT_TIMESTAMP not null comment '创建时间'
+)
+    comment '流程实例';
 
-
-CREATE TABLE `workflow_log`
+create table workflow_data_record
 (
-    `id`          int(11)   NOT NULL AUTO_INCREMENT,
-    `lid`         int(11)   NOT NULL,
-    `did`         int(11)   NOT NULL,
-    `file`        mediumtext COLLATE utf8mb4_unicode_ci,
-    `update_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    PRIMARY KEY (`id`)
-) ENGINE = InnoDB
-  AUTO_INCREMENT = 1
-  DEFAULT CHARSET = utf8mb4
-  COLLATE = utf8mb4_unicode_ci;
+    id              int auto_increment comment '自增主键'
+        primary key,
+    workflow_id     int                                 not null comment '流程id',
+    status          varchar(100)                        not null comment '状态',
+    create_by       varchar(100)                        not null comment '创建人',
+    create_datetime timestamp default CURRENT_TIMESTAMP not null comment '创建时间'
+)
+    comment '流程实例记录';
+
+create table workflow_business_log
+(
+    id              int auto_increment comment '自增主键'
+        primary key,
+    workflow_id     int                                 not null comment '流程id',
+    content         mediumtext                          not null comment '内容',
+    create_by       varchar(100)                        not null comment '创建人',
+    create_datetime timestamp default CURRENT_TIMESTAMP not null comment '创建时间'
+)
+    comment '流程日志';
+
+create table workflow_business_rollback
+(
+    id              int auto_increment
+        primary key,
+    workflow_id     int                                 not null comment '流程id',
+    content         mediumtext                          not null comment '内容',
+    create_datetime timestamp default CURRENT_TIMESTAMP not null comment '创建时间'
+)
+    comment '流程业务回滚';
+
+
+
+
+
+
