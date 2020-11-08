021e18168847e4488e22d5905cc9a401cfc01d20
tristan <tanshilinmail@gmail.com>
2020-11-08 21:18:51 +0800

    添加执行器路由

--- route.py
+++ route.py
@@ -1,19 +1,23 @@
 def init(app):
     from rest import ping
-    # 运维
-    from rest import agent
     from rest.operate.business import data as operate_business_data
     from rest.operate.business import directory as operate_business_directory
     from rest.operate.cmdb import data as operate_cmdb_data
     from rest.operate.cmdb import directory as operate_cmdb_directory
     from rest.operate.cmdb import struct as operate_cmdb_struct
-    # 变更
+    from rest.operate.executor import data as operate_executor_data
+    from rest import agent
 
-    # 注册路由
+    # ################### 注册路由
     app.register_blueprint(ping.app)
-    app.register_blueprint(agent.app)
-    app.register_blueprint(operate_business_data.app)
-    app.register_blueprint(operate_business_directory.app)
+    # 数据模型
     app.register_blueprint(operate_cmdb_data.app)
     app.register_blueprint(operate_cmdb_directory.app)
     app.register_blueprint(operate_cmdb_struct.app)
+    # 业务脚本
+    app.register_blueprint(operate_business_data.app)
+    app.register_blueprint(operate_business_directory.app)
+    # 执行器
+    app.register_blueprint(operate_executor_data.app)
+    # agent
+    app.register_blueprint(agent.app)
