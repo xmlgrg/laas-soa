0e070db29ca858c0d8ec8b008035f8dcda0eac1c
tristan <tanshilinmail@gmail.com>
2020-11-08 21:13:49 +0800

    添加执行器

---
+++ business_hardcode/build_project.py
@@ -0,0 +1,13 @@
+"""
+构建项目
+
+需要依赖一些数据
+    构建服务器
+    源码仓库信息
+    项目配置信息
+"""
+import logging
+
+
+def build_project(data_id):
+    logging.warning("data_id: ", data_id)
---
+++ rest/operate/executor/data.py
@@ -0,0 +1,37 @@
+"""
+执行器 执行业务
+
+暂时为硬编码实现业务
+后续改为流通数据到业务中, 执行业务
+"""
+import json
+import threading
+import time
+
+from flask import Blueprint
+
+from business_hardcode import build_project
+from component import form
+
+app = Blueprint('operate_executor_execute', __name__,
+                url_prefix='/operate/executor/data')
+
+business_hardcode_pool = {
+    "build_project": build_project.build_project,
+}
+
+
+@app.route('/insert', methods=['POST'])
+def insert():
+    """
+    执行业务, 向执行队列中添加一个执行任务
+    :return:
+    """
+    request_data = form.check(['business_id', "data_data_id"])  # 校验是否传入 业务id, 数据模型数据id
+    business_id = request_data["business_struct_id"]
+    data_data_id = request_data["data_data_id"]
+    executor_data_id = time.time_ns()  # 执行器数据id, 后续包含: 执行日志、执行状态 等等
+    threading.Thread(target=business_hardcode_pool[business_id], args=(executor_data_id, data_data_id,)).start()
+    return json.dumps({
+        "executor_data_id": executor_data_id
+    })
