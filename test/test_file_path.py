6ebb90b7dcf3c7575ee8d7bb325ea23aec02148b
tristan <tanshilinmail@gmail.com>
2020-11-09 19:11:56 +0800

    完成同步文件夹到远程服务器

---
+++ test/test_file_path.py
@@ -0,0 +1,15 @@
+from rest.operate.executor import context
+
+local_executor_root_path = r'D:\projects\github\laashub-soa\laas-soa\business_hardcode\build_project'
+remote_basic_dir = "/data/tristan/1"
+
+if __name__ == '__main__':
+    local_files = context.get_local_files(local_executor_root_path + "/data_data")
+    print(local_files)
+    print("-" * 100)
+    remote_files = context.convert_local_files_2_remote_files(local_files, local_executor_root_path, remote_basic_dir)
+    # print(remote_files)
+    for item in local_files:
+        if item.endswith("/"):
+            item = item[:-1]
+            print(item)
