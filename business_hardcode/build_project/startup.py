9836eb7678f289a6215f4ab6cc391cb19ae85734
tristan <tanshilinmail@gmail.com>
2020-11-11 14:04:51 +0800

    temp

--- business_hardcode/build_project/startup.py
+++ business_hardcode/build_project/startup.py
@@ -12,18 +12,26 @@ executor_id = args.executor_id[0]  # 当前执行器id
 root_path = os.getcwd()
 cur_executor_path = root_path + "/" + "run" + "/" + str(executor_id)
 # 加载数据
-# 加载业务脚本, 将部分数据替换到业务脚本中, 执行业务脚本
 startup_data = None
 docker_registry_data = None
 git_server_data = None
+# 加载源码
+finally_project_code_path = None
+finally_project_build_path = None
+# 加载业务脚本, 将部分数据替换到业务脚本中, 执行业务脚本
+business_hyper_fusion_path = None
 """
 {"id": 1, "url": "http://git.xxx.com", "robot_username": "xxx", "robot_password": "xxx", 
 "name": "本地git服务器", "update_datetime": {"$date": 1604953278000}, "create_datetime": {"$date": 1604953278000}}
 """
 
 
-def log(log_content):
-    pass
+def execute_shell(command, is_print=True):
+    print(command)
+    out_log = os.popen(command).read()
+    if is_print:
+        print(out_log)
+    return out_log
 
 
 def load_startup_data():
@@ -53,21 +61,22 @@ def load_project_source_code():
     :return:
     """
     # 判断git指令是否有安装
-    out_log = os.popen("git").read()
+    out_log = execute_shell("git", False)
     if "command not found" in out_log:
         print(out_log)
-        out_log = os.popen("yum install -y git").read()
-        print(out_log)
+        execute_shell("yum install -y git")
     # 判断docker是否有安装
     # 创建目录
     code_path = root_path + "/" + "cache" + "/" + "code"  # 源码目录
     repo_path = startup_data["repo_path"]
     branches = startup_data["branches"]
     repo_path_path = (repo_path[repo_path.find("//") + 2:]).replace(".", "_")  # 仓库目录
-    branches_path = "branches" + "/" + branches + "/" + "source"
-    finally_source_code_path = code_path + "/" + repo_path_path + "/" + branches_path
-    out_log = os.popen("mkdir -p " + finally_source_code_path).read()
-    print(out_log)
+    branches_path = "branches" + "/" + branches
+    global finally_project_code_path
+    global finally_project_build_path
+    finally_project_code_path = code_path + "/" + repo_path_path + "/" + branches_path + "/" + "source"
+    finally_project_build_path = code_path + "/" + repo_path_path + "/" + branches_path + "/" + "build"
+    execute_shell("mkdir -p " + finally_project_code_path)
     # 项目路径目录
     username = git_server_data["robot_username"]
     password = git_server_data["robot_password"]
@@ -76,22 +85,45 @@ def load_project_source_code():
     repo_path_list.insert(repo_path.find("//") + 2, auth_username_password)
     command_repo_path = "".join(repo_path_list) + ".git"
     # git clone http://<username>:<password>@git_server_url/repo_path.git -b <分支名> --single--branch
-    switch_path_command = "cd " + finally_source_code_path + " && "
+    switch_path_command = "cd " + finally_project_code_path + " && "
     # 如果仓库中有.git文件夹的话则进行增量更新, 反之进行全量拉取
-    git_metadata_dir_path = finally_source_code_path + "/" + '.git'
+    git_metadata_dir_path = finally_project_code_path + "/" + '.git'
     if os.path.exists(git_metadata_dir_path) or os.path.isdir(git_metadata_dir_path):  # 如果仓库中有.git文件夹的话则进行增量更新
         command = "git pull origin"
     else:  # 全量拉取
+        # TODO 清理全量拉取目录
         command = "git clone " + " -b " + branches + " --single-branch " + command_repo_path + " ./"
 
     command = switch_path_command + command
     print(command.replace(auth_username_password, ""))
-    out_log = os.popen(command).read()
-    print(out_log)
+    execute_shell(command)
 
 
 def load_business():
-    pass
+    project_program_language = startup_data["program_language"]
+    global business_hyper_fusion_path
+    business_hyper_fusion_path = root_path + "/" + "business_hyper_fusion" + "/" + project_program_language
+    do_build_project_path = business_hyper_fusion_path + "/" + "do_build_project.sh"
+    with open(do_build_project_path) as f:
+        build_project_path_text = f.read()
+        build_project_sh_path = business_hyper_fusion_path + "/" + "build_project.sh"
+
+        build_project_sh_template = """
+        docker run -it \
+          --name=exe1cute_business_1_{execute_id} \
+          -v /data/tristan/1/cache/java:/root/.m2 \
+          -v {finally_project_code_path}:/source_code  \
+          -v {finally_project_build_path}:/source_code/target   \
+          maven:3-alpine {build_project_sh_path}
+        """
+        build_project_sh = build_project_sh_template.format(**{
+            "execute_id": executor_id,
+            "finally_project_code_path": finally_project_code_path,
+            "finally_project_build_path": finally_project_build_path,
+            "build_project_sh_path": build_project_sh_path,
+        })
+        execute_shell("chmod +x " + build_project_sh_path)
+        execute_shell(build_project_sh)
 
 
 def execute_business():
@@ -109,3 +141,4 @@ def execute_business():
 if __name__ == '__main__':
     load_startup_data()
     load_project_source_code()
+    load_business()
